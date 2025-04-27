31b7a7b95ad6a163c2311b177ffc11354a3537ea
Pascal Kiser <pascal.kiser@composed.ch>
2025-04-27 12:48:40 +0200

    fix issue with parsing state

--- src/app/core/mappers/vm.mapper.ts
+++ src/app/core/mappers/vm.mapper.ts
@@ -1,13 +1,18 @@
 import { Vm } from '../models/vm.model';
 import { State } from '../models/state.enum';
 
+function isState(value: any): value is State {
+    return Object.values(State).includes(value);
+}
+
+
 export function mapVm(raw: any): Vm {
     return {
         id: raw.id,
         name: raw.name,
         labels: raw.labels,
         ip: raw.ip,
-        state: raw.state in State ? raw.state as State : State.Error
+        state: isState(raw.state) ? raw.state : State.Error
     };
 }
 
